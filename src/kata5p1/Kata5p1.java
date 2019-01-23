248d7d233ae7cc4c7da837590248108a7ebd07b9
alexvjis2 <alxvj92@gmail.com>
2019-01-23 18:55:25 +0000

    Kata 5 P1 Version 3

--- src/kata5p1/Kata5p1.java
+++ src/kata5p1/Kata5p1.java
@@ -6,18 +6,35 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class Kata5p1 {
 
+    private static final String emails = "email.txt";
+    private static Connection conn = null;
+
     public static void main(String[] args) {
-        createTable();
+        connect();
+        insert(MailListReader.read(emails));
+    }
+
+    private static void insert(List<String> emails) {
+        String sql = "INSERT INTO EMAIL ( Mail ) VALUES ( ? )";
+
+        try ( PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (String email : emails) {
+                pstmt.setString(1, email);
+                pstmt.executeUpdate(); 
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
     }
 
     public static void selectAll() {
         String query = "SELECT * FROM PEOPLE";
-        try (Connection conn = connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(query)) {
+        try ( Statement stmt = conn.createStatement();
+              ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
                 System.out.print(rs.getInt("id") + "\t");
@@ -38,21 +55,22 @@ public class Kata5p1 {
                 + " ID integer PRIMARY KEY AUTOINCREMENT,\n"
                 + " Mail text NOT NULL);";
 
-        try (Connection conn = connect();
-            Statement stmt = conn.createStatement()) {
+        try (Statement stmt = conn.createStatement()) {
             return stmt.execute(sql);
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
-        
+
         return false;
     }
 
-    private static Connection connect() throws SQLException {
-        Connection conn = null;
-        String url = "jdbc:sqlite:KATA5.db";
-        conn = DriverManager.getConnection(url);
-        return conn;
+    private static void connect() {
+        try {
+            String url = "jdbc:sqlite:KATA5.db";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
     }
 
 }
